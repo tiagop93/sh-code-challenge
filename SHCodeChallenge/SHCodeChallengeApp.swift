20579e589218443799e9e3c6021e05b5c021cf17
Tiago Pereira <tiago_pereira_93@hotmail.com>
2024-10-29 12:37:19 +0000

    Initial Commit

---
+++ SHCodeChallenge/SHCodeChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SHCodeChallengeApp.swift
+//  SHCodeChallenge
+//
+//  Created by Tiago Pereira on 29/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct SHCodeChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
